52c83455c37a24606842e2253c7ca0c86ca2bab9
Andrzej Borucki <borucki.andrzej@gmail.com>
2023-03-15 16:40:58 +0100

    fixed small problems

--- wwi.py
+++ wwi.py
@@ -1,19 +1,20 @@
 import os
 
-from mysql.connector import connect, Error
+fix smafrom mysql.connector import connect
 from getpass import getpass
 import configparser
 import spacy as spacy
 
-wikilang = "en"
-if wikilang == "en":
-    spacy_name = wikilang + '_core_web_md'
+wikiLang = "en"
+if wikiLang == "en":
+    spacy_name = wikiLang + '_core_web_md'
 else:
-    spacy_name = wikilang + 'en_core_news_md'
+    spacy_name = wikiLang + 'en_core_news_md'
 
 
 def spacy_load():
     nlp = spacy.load(spacy_name, disable='ner')
+
 
 def safe_spacy_load():
     try:
@@ -24,71 +25,74 @@ def safe_spacy_load():
 
 # safe_spacy_load()
 
+
 def read_credentials():
     config = configparser.ConfigParser()
     config.read('wwi.ini')
     if 'user' in config['credentials']:
-        user=config['credentials']['user']
+        user = config['credentials']['user']
     else:
-        user=None
+        user = None
     if 'password' in config['credentials']:
-        password=config['credentials']['password']
+        password = config['credentials']['password']
     else:
-        password=None
+        password = None
     if not user:
-        user=input("Enter username (or put it to wwi.ini): "),
+        user = input("Enter username (or put it to wwi.ini): "),
     if not password:
-        password=getpass("Enter password (or put it in plain text to wwi.ini): "),
-    return(user,password)
+        password = getpass("Enter password (or put it in plain text to wwi.ini): "),
+    return user, password
 
 
-def connectWiki():
+def connect_wiki():
     credentials = read_credentials()
     return connect(
         host="localhost",
         user=credentials[0],
         password=credentials[1],
-        database="wiki_"+wikilang,
+        database="wiki_" + wikiLang,
     )
 
-def getSqlFromFile(path):
+
+def get_sql_from_file(path):
     file = open(path, 'r')
     lines = file.readlines()
     file.close()
-    for i in range(0,len(lines)):
-         lines[i]=lines[i].strip()
-    sql_list=list()
+    for i in range(0, len(lines)):
+        lines[i] = lines[i].strip()
+    sql_list = list()
     sql = ""
-    for i in range(0,len(lines)):
+    for i in range(0, len(lines)):
         sql += " " + lines[i]
         if lines[i].endswith(';'):
             sql_list.append(sql)
             sql = ""
-    if len(sql)>0:
+    if len(sql) > 0:
         sql_list.append(sql)
     return sql_list
 
-def drop_and_create_tables(connection):
-    dir = 'sql/tables'
-    dirlist = os.listdir(dir)
+
+def drop_and_create_tables(conn):
+    directory = 'sql/tables'
+    dirlist = os.listdir(directory)
     # Tables have constraints so drop and create must be in specified order
     dirlist.sort(reverse=True)
-    for fname in dirlist:
-        if not fname.endswith('.sql'):
+    for filename in dirlist:
+        if not filename.endswith('.sql'):
             continue
-        stmtDrop = getSqlFromFile(dir+'/'+fname)[0]
-        with connection.cursor() as cursor:
-            cursor.execute(stmtDrop)
+        stmt = get_sql_from_file(directory + '/' + filename)[0]
+        with conn.cursor() as cursor:
+            cursor.execute(stmt)
 
     dirlist.sort()
-    for fname in dirlist:
-        if not fname.endswith('.sql'):
+    for filename in dirlist:
+        if not filename.endswith('.sql'):
             continue
-        stmtCreate = getSqlFromFile(dir + '/' + fname)[1]
-        with connection.cursor() as cursor:
-            cursor.execute(stmtCreate)
+        stmt = get_sql_from_file(directory + '/' + filename)[1]
+        with conn.cursor() as cursor:
+            cursor.execute(stmt)
 
 
-connection = connectWiki()
+connection = connect_wiki()
 drop_and_create_tables(connection)
 connection.close()
