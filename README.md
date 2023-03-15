f7e2bf121c595c7501b1dd569f5958e7dbdd767b
Andrzej Borucki <borucki.andrzej@gmail.com>
2023-03-15 14:30:37 +0100

    read credentials: user and password from ini or terminal

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # wiki-word-index
 Word indexer for WIkipedia dumps
+
+Run with PyCharm or source venv/bin/activate <br>
+in Pycharm password will be echoed
--- wwi.py
+++ wwi.py
@@ -1,3 +1,5 @@
+from getpass import getpass
+import configparser
 import spacy as spacy
 
 wikilang = "en"
@@ -17,5 +19,25 @@ def safe_spacy_load():
         spacy.cli.download(spacy_name)
         spacy_load()
 
+# safe_spacy_load()
 
-safe_spacy_load()
+def read_credentials():
+    config = configparser.ConfigParser()
+    config.read('wwi.ini')
+    if 'user' in config['credentials']:
+        user=config['credentials']['user']
+    else:
+        user=None
+    if 'password' in config['credentials']:
+        password=config['credentials']['password']
+    else:
+        password=None
+    if not user:
+        user=input("Enter username (or put it to wwi.ini): "),
+    if not password:
+        password=getpass("Enter password (or put it in plain text to wwi.ini): "),
+    return(user,password)
+
+
+(user,password) = read_credentials()
+print(user,password)
